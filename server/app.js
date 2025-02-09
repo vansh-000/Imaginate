62c2a85f0c401e947cf413c9a4ee6164bd07dd32
vansh-000 <23165@iiitu.ac.in>
2025-02-09 19:28:08 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -5,9 +5,16 @@ import cookieParser from 'cookie-parser';
 import imageRouter from "./routes/image.routes.js";
 
 const app = express();
+
+app.use(cors({
+  origin: "https://imaginate-ten.vercel.app",
+  credentials: true 
+}));
+
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+
 app.use("/api/user", userRouter);
 app.use("/api/image", imageRouter);
+
 export { app };
